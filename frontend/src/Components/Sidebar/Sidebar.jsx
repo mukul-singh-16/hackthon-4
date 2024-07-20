d4970da39b0c076bc3bf2fb00cdc3136532fee2e
Ajay Tyagi <ajay0120118@gmail.com>
2024-07-20 15:20:58 +0530

    new features updated

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -25,9 +25,9 @@ const Sidebar = ({ sidebarToggle }) => {
       <hr />
       <ul className="mt-3 text-white font-bold">
         <li className="mb-2 rounded hover:shadow hover:bg-blue-500 py-2">
-          <Link href="" className="px-3" onClick={() => { setvalue("CropsSutablity") }} >
+          <Link href="" className="px-3" to="/" >
             <FaHome className="inline-block w-6 h-6 mr-2 -mt-2"></FaHome>
-            HealthSutablity
+            Home
           </Link>
         </li>
       </ul>
